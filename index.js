25f80381c6a064cddb2f541ddf7e2dff2bb09e89
Wobi Nurdiansyah <wobinurdiansyah20@gmail.com>
2024-12-19 23:42:03 +0700

    on progres Dom js and resvonsive

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("form");
+const Alertspan = document.querySelector("span");
+const form = document.getElementById("form");
+const form2 = document.getElementById("form2");
 
-  form.addEventListener("submit", function (e) {
+const pass3 = document.getElementById("password");
+
+document.addEventListener("DOMContentLoaded", function () {
+  form2.addEventListener("submit", function (e) {
     e.preventDefault();
     const formdata = new FormData(form);
     const getEmail = formdata.get("email");
     const getPass = formdata.get("password");
-    const getData = localStorage.getItem('userData.email')
-    const getpass = localStorage.getItem('userData.pass')
     console.log(getEmail);
     console.log(getPass);
-    if (getEmail === 'admin@mail' && getPass === '123') {
+    if (getEmail === "admin@mail" && getPass === "123") {
       location.href = "Landingpage2.html";
       return;
     } else {
       const call = document.querySelector("#alert2>div");
       call.style.visibility = "visible";
     }
+    console.log(getEmail);
+    if (getEmail === "admin@mail") {
+      location.href = "enterpin1.html";
+    } else {
+      Alertspan.style.visibility = "visible";
+    }
+    //
+    const userData = {
+      email: email,
+      pass1: pass1,
+      pass2: pass2,
+    };
+    localStorage.setItem("userData", JSON.stringify(userData));
+    console.log(userData);
+    alert("Data berhasil disimpan ke localStorage!");
+
+    console.log(getEmail);
     const json = JSON.stringify(formdata).toString();
     console.log(json);
   });
 });
-
+//
 function validationPass(pass) {
   const regexPass =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+={}\[\]:;"'<>,.?~`-]).{8,}$/;
@@ -33,7 +52,6 @@ function validationPass(pass) {
   // $ : Menandakan akhir dari string.
   return regexPass.test(pass);
 }
-
 
 const form2 = document.getElementById("form2");
 form2.addEventListener("submit", function (e) {
@@ -48,6 +66,14 @@ form2.addEventListener("submit", function (e) {
     console.log(email);
     console.log(pass1);
     console.log(pass2);
+    if (email === "" && pass1 === "" && pass2) {
+      location.href = "Landingpage2.html";
+      return;
+    } else {
+      const call = document.querySelector("#alert2>div");
+      call.style.visibility = "visible";
+    }
+
     if (validationPass(pass1)) {
       alertfailed.style.visibility = "visible";
     }
@@ -63,7 +89,20 @@ form2.addEventListener("submit", function (e) {
       pass2: pass2,
     };
     localStorage.setItem("userData", JSON.stringify(userData));
-
+    console.log(userData);
     alert("Data berhasil disimpan ke localStorage!");
   }, 1000);
 });
+
+function change() {
+  const pass3 = document.getElementById("password");
+  if (pass3.type === "password") {
+    pass3.type = "text";
+    return;
+  }
+  console.log("masuk");
+  // pass.te
+  pass3.type = "password";
+}
+//
+//
